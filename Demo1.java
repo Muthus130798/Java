f9456ebd9476541f9c414380de1a7f36a0698965
Muthu S <109411339+Muthus130798@users.noreply.github.com>
2023-06-27 15:53:26 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,6 @@
+class Demo1{
+	static void main(String[] args)
+	{
+		System.out.println ("Hello");
+	}
+}
---
+++ Demo2.java
@@ -0,0 +1,6 @@
+class Demo2{
+	public void main(String[] args)
+	{
+		System.out.println ("Hello");
+	}
+}
